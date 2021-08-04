c9ea912a491e29c70768d686e02534e40e7c9162
Luciano Lanes Lopes <llaneslopes@gmail.com>
2021-08-04 16:03:03 -0300

    Requisitos 12 e 13 concluidos

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
 
 class Feedback extends React.Component {
+  constructor() {
+    super();
+    this.renderFeedbackMessage = this.renderFeedbackMessage.bind(this);
+  }
+
+  renderFeedbackMessage() {
+    const { userScore: {score} } = this.props;
+    const hit = 3;
+    if (score >= hit) {
+      return (
+        <p data-testid="feedback-text">Mandou bem!</p>
+      );
+    }
+    return (<p data-testid="feedback-text">Podia ser melhor...</p>);
+  }
+
   render() {
     return (
-      <h1>Eu sou o Feedback</h1>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
 
-export default Feedback;
+const mapStateToProps = (state) => ({
+  userScore: state.rootReducer.score,
+});
+
+Feedback.propTypes = {
+  userInfo: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+
+export default connect(mapStateToProps)(Feedback);
+
+// A mensagem deve ser "Podia ser melhor..." caso a pessoa acerte menos de 3 perguntas
+// A mensagem deve ser "Mandou bem!" caso a pessoa acerte 3 perguntas ou mais
+// O elemento da mensagem de feedback deve possuir o atributo data-testid com o valor feedback-text
