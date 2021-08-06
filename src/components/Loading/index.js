b616735727ad84fd5e4656868d494433b62c9068
tiagoornelas <advtiagoornelas@gmail.com>
2021-08-06 19:44:55 -0300

    Estiliza a tela de loading e inicia a estilização da tela de login

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import loadingGif from '../../img/loading.gif';
 import './style.css';
 
 class Loading extends React.Component {
   render() {
     return (
-      <h3 className="loading">Carregando...</h3>
+      <div className="loadingContainer">
+        <h3 className="loading">Carregando...</h3>
+        <img className="loadingGif" src={ loadingGif } alt="loading" />
+      </div>
     );
   }
 }
