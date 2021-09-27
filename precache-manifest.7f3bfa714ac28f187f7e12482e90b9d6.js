ce2795acf960a53483beb2163aa0ee011f600711
Luciano Lanes <llaneslopes@gmail.com>
2021-09-27 11:30:50 -0300

    Updates

---
+++ precache-manifest.7f3bfa714ac28f187f7e12482e90b9d6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb21c19365f16a54bbc65b3e3c2f032",
+    "url": "/trivia-react-redux/index.html"
+  },
+  {
+    "revision": "6fd4460bdf2e5958aeab",
+    "url": "/trivia-react-redux/static/css/main.ef090a8e.chunk.css"
+  },
+  {
+    "revision": "d76fa8b975a4d5a84927",
+    "url": "/trivia-react-redux/static/js/2.7b395c4c.chunk.js"
+  },
+  {
+    "revision": "3f6aa4d1b94e64ba93470af92b79dbf5",
+    "url": "/trivia-react-redux/static/js/2.7b395c4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6fd4460bdf2e5958aeab",
+    "url": "/trivia-react-redux/static/js/main.f5cbd97c.chunk.js"
+  },
+  {
+    "revision": "c9265d93de189acb100f",
+    "url": "/trivia-react-redux/static/js/runtime-main.460e2238.js"
+  },
+  {
+    "revision": "236428001ad64d6b4b4c5f3c0fce6fdb",
+    "url": "/trivia-react-redux/static/media/background.23642800.jpg"
+  },
+  {
+    "revision": "c9c50958c550682c2baacfb6918989f3",
+    "url": "/trivia-react-redux/static/media/bronze.c9c50958.png"
+  },
+  {
+    "revision": "c922d44ecb4b53edf7a7632a814d8fed",
+    "url": "/trivia-react-redux/static/media/error.c922d44e.png"
+  },
+  {
+    "revision": "6c75b9d2541066311c8b0f50c4a09219",
+    "url": "/trivia-react-redux/static/media/gold.6c75b9d2.png"
+  },
+  {
+    "revision": "f89c84a7fec2895efa76c2876289aee9",
+    "url": "/trivia-react-redux/static/media/loading.f89c84a7.png"
+  },
+  {
+    "revision": "1ae812891f66e9d0f8d401fd52746c5c",
+    "url": "/trivia-react-redux/static/media/purple-rain.1ae81289.gif"
+  },
+  {
+    "revision": "385d34f6a2e75fff3ee6a5bedf05262e",
+    "url": "/trivia-react-redux/static/media/purple_rain.385d34f6.mp3"
+  },
+  {
+    "revision": "74cafa2eb9d5e6a7c754febd3bc24bec",
+    "url": "/trivia-react-redux/static/media/purple_rain_lyrics.74cafa2e.vtt"
+  },
+  {
+    "revision": "3178060b83732e3b1c8bd27f4db02e30",
+    "url": "/trivia-react-redux/static/media/silver.3178060b.png"
+  },
+  {
+    "revision": "5ca37e73090db1ca80816000607807a1",
+    "url": "/trivia-react-redux/static/media/trivia.5ca37e73.png"
+  }
+]);
